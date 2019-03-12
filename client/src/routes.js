e68e39c06ca28c02a5a82414739209b9dedc5efa
milktegi <milkteegi@gmail.com>
2019-03-12 09:45:29 +0900

    all

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,10 +5,13 @@ import Layout from './hoc/layout';
 import Home from './components/Home';
 import RegisterLogin from './components/Register_login';
 
+import Register from './components/Register_login/register';
+
 const Routes = () => {
 	return(
 		<Layout>
 			<Switch>
+				<Route path="/register" exact component={Register}/>
 				<Route path="/register_login" exact component={RegisterLogin}/>
 				<Route path="/" exact component={Home}/>
 			</Switch>
